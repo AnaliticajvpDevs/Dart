e42857355b6d6a157e58c1ebc7ca3e37888e00b2
Juan Meregone <jmeregone@jvpconsultores.com.ar>
2022-04-21 20:54:42 -0300

    adding datos

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+//import 'package:dart_mssql/dart_mssql.dart';
 import 'dart:convert';
 
 main() async {
@@ -15,28 +16,32 @@ main() async {
       reg.add(item);
     }
   }
-  cliente(reg[120]);
+  for (var i = 50; i < 70; i++) {
+    cliente(reg[i]);
+  }
 }
 
 cliente(cli) {
-  var idobjeto      = cli['o'];
-  var lat           = cli['v'][0].split('|')[0];
-  var lon           = cli['v'][0].split('|')[1];
-  var nombre        = findVal(cli, '@oName');
-  var presion       = findVal(cli, '@presion');
-  var auditoria     = findVal(cli, "@estadoaud");
+  var idobjeto = cli['o'];
+  var lat = cli['v'][0].split('|')[0];
+  var lon = cli['v'][0].split('|')[1];
+  var nombre = findVal(cli, '@oName');
+  var presion = findVal(cli, '@presion');
+  var auditoria = findVal(cli, "@estadoaud");
   var mantenimiento = findVal(cli, "@estadomant");
-  var sector        = findVal(cli, '@sector');
+  var sector = findVal(cli, '@sector');
 
-  print(idobjeto);
-  print(lat);
-  print(lon);
-  print(nombre);
-  print(presion);
-  print(auditoria);
-  print(mantenimiento);
-  print(sector);
+  print("Id del objeto: $idobjeto");
+  print("Latitud: $lat");
+  print("Longitud: $lon");
+  print("Nombre: $nombre");
+  print("Presion: $presion");
+  print("Auditoria: $auditoria");
+  print("Mantenimiento: $mantenimiento");
+  print("Sector: $sector");
+  print("");
 }
+
 findVal(registro, name) {
   for (var item in registro['val']) {
     if (item.split('|')[0] == name) {
