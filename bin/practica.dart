be1b2c041ae9dfcebf3d04d0195a6c07bba40e6f
Juan Meregone <jmeregone@jvpconsultores.com.ar>
2022-04-25 21:24:44 -0300

    practica

---
+++ bin/practica.dart
@@ -0,0 +1,18 @@
+var datos = {
+  "ac": "Acometida",
+  "b": "EEA",
+  "de": "Nodo",
+  "e": "Valvulas Cierre",
+  "erclo": "Erclo",
+  "h": "Hidrante",
+  "p": "Pozo",
+  "pl": "Planta",
+  "t": "Tanque",
+  "va": "Valvula Aire"
+};
+
+main() {
+  if(datos['ac'] == "Acometida"){
+    print('correcto');
+  }  
+}
